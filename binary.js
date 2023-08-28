0e6cb7e2120f5393fa5a086df26ebd66303fff3f
Negrea-Andrei <negrea97@yahoo.com>
2023-08-28 21:08:51 +0300

    create the tree class

--- binary.js
+++ binary.js
@@ -9,7 +9,42 @@ class newNode {
 }
 
 class tree {
-    constructor() {
-
+    constructor(array) {
+        this.root = null
+        this.sortedArray = [...new Set(array)].sort((a, b) => a - b);
+        this.root = this.treeBuildingFunction(this.sortedArray)
     }
+
+    treeBuildingFunction(array) {
+        if(array.length === 0) {
+            return null
+        }
+
+        const mid = Math.floor(array.length / 2);
+        const root = new newNode(array[mid])
+        root.left = this.treeBuildingFunction(array.slice(0, mid))
+        root.right = this.treeBuildingFunction(array.slice(mid+1));
+        return root
+    }
+
+    prettyPrint(node = this.root, prefix = "", isLeft = true) {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    }
+
 }
+
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const treeTest = new tree(testArray)
+
+console.log([...new Set(testArray)].sort((a, b) => a - b))
+
+treeTest.prettyPrint()
